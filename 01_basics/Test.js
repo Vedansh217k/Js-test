e9c63adbfd33e072c02cf2178e199a49475b08d8
Vedansh217k <vedanshkumar217@gmail.com>
2024-12-25 19:03:02 +0530

    Variables

--- 01_basics/Test.js
+++ 01_basics/Test.js
@@ -1,1 +1,17 @@
-console.log("Vedansh Kumar")
+const accountId = 123456
+let accountEmail = "azxdfgvbn@google.com"
+var accounPasswaord = "123456"
+accountCity = "Paris"
+let acccountState;
+
+// accountId = 1234 Not allowed
+accountEmail = "asedcfgbn@google.com"
+accounPasswaord = "123456"
+accountCity = "Spain"
+console.log(accountId);
+/*
+
+prefer not to us evar 
+because of issue in block socpe and funtional scope
+*/
+console.table([accountEmail,accounPasswaord,accountCity,acccountState])
