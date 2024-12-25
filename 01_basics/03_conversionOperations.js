b9a4eccc0d1024858a53abf58a7a6d39d25b0f22
Vedansh217k <vedanshkumar217@gmail.com>
2024-12-25 20:43:43 +0530

    Conversion Operation

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,20 @@
+let score= "37"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber );
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// True => 1 ; false => 0
+let isLoggedIn = "Sam"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+let someNumber =33
+let stringNumber = String(someNumber); 
+console.log(stringNumber);
+console.log(typeof stringNumber);
