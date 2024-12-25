31ae58eae319dbf948aee61fa856cb4422b0e28b
Vedansh217k <vedanshkumar217@gmail.com>
2024-12-25 19:50:24 +0530

    Datatypes

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+7) // we are using nodejs , not browser
+
+let name = "Sam"
+let age = 21
+let isLoggedIn = false 
+let statel;
+
+
+// number=> 2 to power 53
+//bigint 
+//string => ""
+//boolean =>true/false
+//null =>stanalone value
+//undefined =>
+//symbol =>unique
+
+
+//object
+console.log(typeof "sam");
