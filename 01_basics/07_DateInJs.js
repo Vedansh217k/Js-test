0e79a2b35396a6b14deaa0c91a2d1ccdce7239eb
Vedansh217k <vedanshkumar217@gmail.com>
2024-12-27 18:26:50 +0530

    Dates

---
+++ 01_basics/07_DateInJs.js
@@ -0,0 +1,22 @@
+//Dates
+
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+
+// let myCreatedDate = new Date(2024, 0, 27)
+// let myCreatedDate = new Date(2024, 0, 27, 5, 3)
+// let myCreatedDate = new Date("2024-00-16")
+// let myCreatedDate = new Date("27-12-2024")
+
+
+
+
+
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(Date.now());
